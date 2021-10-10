b44182eeb5e1f65cacdb458e568f108417b20079
magnokf <mkfelipe.rj@gmail.com>
2021-10-10 10:49:35 -0300

    feat: organizar seeder

--- laravel/database/seeders/UsersAndNotesSeeder.php
+++ laravel/database/seeders/UsersAndNotesSeeder.php
@@ -34,27 +34,27 @@ class UsersAndNotesSeeder extends Seeder
             'role_id' => $adminRole->id,
             'hierarchy' => 1,
         ]);
-        $diretorRole = Role::create(['name' => 'diretor']);
+        $diretorRole = $roleDiretor = Role::create(['name' => 'diretor']);
         RoleHierarchy::create([
             'role_id' => $diretorRole->id,
             'hierarchy' => 2,
         ]);
-        $pologestorRole = Role::create(['name' => 'pologestor']);
+        $pologestorRole = $rolePoloGestor = Role::create(['name' => 'pologestor']);
         RoleHierarchy::create([
             'role_id' => $pologestorRole->id,
             'hierarchy' => 3,
         ]);
-        $instrutorRole = Role::create(['name' => 'instrutor']);
+        $instrutorRole = $roleInstrutor = Role::create(['name' => 'instrutor']);
         RoleHierarchy::create([
             'role_id' => $instrutorRole->id,
             'hierarchy' => 4,
         ]);
-        $userRole = Role::create(['name' => 'user']);
+        $userRole = $roleUser = Role::create(['name' => 'user']);
         RoleHierarchy::create([
             'role_id' => $userRole->id,
             'hierarchy' => 5,
         ]);
-        $guestRole = Role::create(['name' => 'guest']);
+        $guestRole = $roleGuest = Role::create(['name' => 'guest']);
         RoleHierarchy::create([
             'role_id' => $guestRole->id,
             'hierarchy' => 6,
@@ -90,13 +90,52 @@ class UsersAndNotesSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'menuroles' => 'user,admin',
+            'menuroles' => 'admin',
             'status' => 'Active'
         ]);
-        $user->assignRole('user');
         $user->assignRole($roleAdmin);
+
+        /*  insert diretor user   */
+        $user = User::create([
+            'name' => 'diretor',
+            'samaccountname' => '2222',
+            'email' => 'diretor@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'menuroles' => 'diretor',
+            'status' => 'Active'
+        ]);
+        $user->assignRole($roleDiretor);
+
+        /*  insert pologestor user   */
+        $user = User::create([
+            'name' => 'pologestor',
+            'samaccountname' => '3333',
+            'email' => 'pologestor@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'menuroles' => 'pologestor',
+            'status' => 'Active'
+        ]);
+        $user->assignRole($rolePoloGestor);
+
+        /*  insert instrutor user   */
+        $user = User::create([
+            'name' => 'instrutor',
+            'samaccountname' => '4444',
+            'email' => 'instrutor@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'menuroles' => 'instrutor',
+            'status' => 'Active'
+        ]);
+        $user->assignRole($roleInstrutor);
+
         for($i = 0; $i<$numberOfUsers; $i++){
-            $a = array('diretor','instrutor','pologestor');
+            $a = array('diretor','instrutor','pologestor','user');
             $n = 1;
             $user = User::create([
                 'name' => $faker->name(),
@@ -108,7 +147,7 @@ class UsersAndNotesSeeder extends Seeder
                 'menuroles' => array_rand(array_flip($a), $n),
                 'status' => $userStatus[ random_int(0,count($userStatus) - 1) ]
             ]);
-            $user->assignRole('user');
+            $user->assignRole(array_rand(array_flip($a), $n));
             array_push($usersIds, $user->id);
         }
 
