ecbf8021476ac160e785d5755e9c159a9c9d88c7
magnokf <mkfelipe.rj@gmail.com>
2021-10-10 00:34:34 -0300

    feat: Ldap Ausente - Resolvido

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -6,9 +6,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
+use LdapRecord\Laravel\Auth\ListensForLdapBindFailure;
 
 class AuthController extends Controller
 {
+    use ListensForLdapBindFailure;
     /**
      * Create a new AuthController instance.
      *
@@ -52,7 +54,7 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $credentials = request(['samaccountname', 'password']);
+        $credentials = $this->credentials($request);
 
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Proibido'], 401);
@@ -102,5 +104,17 @@ class AuthController extends Controller
             'expires_in' => auth()->factory()->getTTL() * 60,
             'roles' => $user->roles,
         ]);
+    }
+
+    protected function credentials(Request $request)
+    {
+        return [
+            'samaccountname' => $request->samaccountname,
+            'password' => $request->password,
+            'fallback' => [
+                'samaccountname' => $request->samaccountname,
+                'password' => $request->password,
+            ],
+        ];
     }
 }
